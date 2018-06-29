824df2d35a73d939ec262537d67e3c0592d74183
julia <juliajessicasher@gmail.com>
2018-06-29 07:23:59 -0700

    create initial project folder

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App(){
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
