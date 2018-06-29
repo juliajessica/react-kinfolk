276cfe87510eaa3aa63ba58b92d9d0c97318da9e
julia <juliajessicasher@gmail.com>
2018-06-29 12:02:42 -0700

    add database for articles

--- src/components/ArticleData.jsx
+++ src/components/ArticleData.jsx
@@ -1,6 +1,68 @@
+import art1 from '../assets/img/art1.jpg';
+import art2 from '../assets/img/art2.jpg';
+import art3 from '../assets/img/art3.jpg';
+import art4from '../assets/img/art4.jpg';
+import art5 from '../assets/img/art5.jpg';
+import art6 from '../assets/img/art6.jpg';
+import art7 from '../assets/img/art7.jpg';
+
 const articleData = [
   {
+    title: "The New Democrats",
+    description: 'In Canada, brothers Jagmeet and Gurratan Singh are redressing the stereotype of “image-conscious” politicians.',
+    image: art1,
+    fullDescription: "It\’s during a break in our photo shoot at the Toronto Reference Library with Jagmeet Singh, the newly minted leader of Canada’s left-wing New Democratic Party, and his brother Gurratan, that it dawns on me—Jagmeet has the job I wanted when I was nine years old."
+  },
+  {
+    title: "Studio David Thulstrup",
+    description: "The Danish designer behind Noma’s new interiors.",
+    image: art2,
+    fullDescription: 'David Thulstrup is coming into his stride. Nine years after opening his eponymous studio in Copenhagen, having previously held positions at Jean Nouvel in Paris and Peter Marino in New York, Thulstrup and his team have gained a reputation for upending the dogmata of Danish design. His latest project—the interior architecture of the recently reopened restaurant Noma—was the perfect collaboration: a designer with his eye on the future joining forces with the pioneers of New Nordic cuisine. Here, Thulstrup reflects on how Danish design lost its colorful flair and explains why he teamed up with Brdr. Krüger, a century-old Danish design house, to produce his new Arv collection of furniture.
 
+Is the way that a project looks ever an indication of how “good” it is?
+\“Good\” definitely goes beyond looks. It’s all about how a space makes you feel. It’s easy to do something that looks pretty, but there has to be functionality and that can be challenging for the eye. I always put the project first, but we do live in a time of social media, and communication about what one does is extremely important. It\’s definitely something I think about every time I do a project.
+
+What did you learn from working with Jean Nouvel?
+The importance of experiencing design—looking at who is going to live in what I\’m doing, and how. Where does the soap go? Do you sleep on the left side of the bed or on the right, for example? Details were something that Jean really pushed, along with working with materiality to do so—leather walls, or marble ceilings.
+
+How do you design your own way of living?
+I reflect on my everyday life and look at my habits. Sometimes I\’m conflicted because I like it very clean and minimal, but I also love all the things that make a house a home: the textures, the softness, the weirdness.'
+  },
+  {
+    title: "Métro, Boulot, Disco",
+    description: "The enduring appeal of Nouvelle Vague.",
+    image: art3,
+    fullDescription: "This story appears in a print issue of Kinfolk. You’re welcome to read three stories from each print issue of Kinfolk for free. To continue reading this story, click here. If you'd like to enjoy unlimited access to our online archive, subscribe here. If you’re already a subscriber, please sign in. Alternatively, keep browsing Kinfolk.com to enjoy more free content."
+  },
+  {
+    title: "Septime",
+    description: "A Parisian restaurant serving up haute-cuisine without the pretention.",
+    image: "https://kinfolk.com/wp-content/uploads/2018/04/Kinfolk_ParisCityGuide_Septime_002-683x1024.jpg",
+    fullDescription: "Septime may regularly rank as one of the world’s best restaurants, but it carries with it none of the stuffiness of its counterparts. Housed in an unmarked street-level building in the 11th Arrondissement, and furnished with wooden chairs and tables, the restaurant has made a name for itself on the back of an unpretentious menu featuring sustainably-sourced ingredients in unusual combinations. Graphic designer-turned-chef Bertrand Grébaut is part of a younger cohort of cooks keen to throw off the shackles that sometimes constrain traditional French cuisine: Lobster with wild strawberries and poached egg in hay broth have both featured on the (frequently changing) menu."
+  },
+  {
+    title: "Michaela DePrince",
+    description: "The young ballerina dancing all over the stereotypes of a pressure-intense career.",
+    image: "https://kinfolk.com/wp-content/uploads/2018/02/Michaela_DePrince_01-822x1024.jpg",
+    fullDescription: "As an orphan growing up during Sierra Leone’s brutal civil war, Michaela DePrince found a magazine blowing in the wind. On its cover was a ballerina and a new inspiration. Now, at the age of 22, Michaela is immersed in the world of ballet. She has also danced with Beyoncé, written a book, and is currently working on a movie based on her life. But hers is not a fairy tale. It is a story of resilience in the face"
+  },
+  {
+    title: "The Photographer in the Garden",
+    description: "Gardens have provided powerful inspiration for Man Ray, Mapplethorpe and a million hobby photographers.",
+    image: "https://kinfolk.com/wp-content/uploads/2018/04/The_Photographer_in_the_Garden_04-851x1024.jpg",
+    fullDescription: "A garden is made of equal parts nature and artifice: It springs from the earth, and yet is defined by human intervention.This combination of opposing forces is precisely the same that powers photography. “In a way [a garden] is a set,” writes Jamie M. Allen in The Photographer in the Garden. Co-authored by Sarah Anne McNear, it is the first book to explore this synchronicity in depth. Flowers have their own language and, as The Photographer in the Garden elegantly demonstrates, that language can be used to convey almost anything."
+  },
+  {
+    title: "13 Bonaparte",
+    description: "Prize-winning photographer Allyssa Heuze on her collaboration with 13 Bonaparte.",
+    image: "https://kinfolk.com/wp-content/uploads/2018/05/13-Bonaparte-Le-Denim-2018-03-819x1024.jpg",
+    fullDescription: "Within the space of one week, emerging photographer Allyssa Heuze won the prestigious Wallpaper Prize at Hyères Festival in France and launched a new series in collaboration with Parisian label 13 Bonaparte. Here, she muses on her approach (don’t shoot straight) and offers advice to other young photographers. 13 Bonaparte really likes to collaborate with young photographers for its Le Denim campaigns. They gave me carte blanche, so I could really do what I wanted and try to balance our two different universes. I recreated an empty apartment because any new collection is also kind of a new beginning. I’m always looking to play with what is usually considered mundane. So, the set is very minimal—just a carpet and a table and chair—but there’s also that fun sense of nostalgia that you get whenever you start something new."
+  },
+  {
+    title: "Jamieson Webster",
+    description: "A conversation with a New York psychoanalyst."
+    image: "https://kinfolk.com/wp-content/uploads/2018/01/Jamieson_Webster-819x1024.jpg",
+    fullDescription: "Psychoanalytic terms—including such pop favorites as “repression, ” “Oedipal complex” and “narcissism”—are part of the vernacular, and yet psychoanalysis as a profession remains maligned. But analyst Jamieson Webster, who practices in Manhattan’s SoHo neighborhood, is fine with that. A professor at the New School as well as a cultural critic, Webster agrees with Freud, who believed that since psychoanalysis concerns things that make us uncomfortable, it will always face resistance. Here, Webster talks about the benefits of forgetting, the myths"
   }
 ];
 
