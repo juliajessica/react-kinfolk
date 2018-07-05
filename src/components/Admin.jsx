3e1da43f7b519525a1d2c4072f47524a3491f6b1
julia <juliajessicasher@gmail.com>
2018-07-04 17:54:48 -0700

    Add props in the admin component

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Admin(){
+function Admin(props){
   return(
     <div>
       <h1>admin is working</h1>
     </div>
   );
+}
+
+Admin.propTypes = {
+  shoppingList: PropTypes.array
 }
 
 export default Admin;
